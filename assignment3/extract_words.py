2425fca46bf041a35b22a507639d58583cbe87b5
EleanorYoung <e.elle.young@gmail.com>
2020-09-22 22:20:53 -0500

    added extract_words.py and resulting .txt files

---
+++ assignment3/extract_words.py
@@ -0,0 +1,49 @@
+import io
+import re
+from collections import defaultdict
+
+def main():
+	#	import and read txt file
+	#	saving grace reference: https://stackoverflow.com/questions/17912307/u-ufeff-in-python-string
+	f = io.open("fairytales.txt", mode="r", encoding="utf-8-sig")
+	txt = f.read()
+	f.close()
+
+	#	format text and make into a list
+	#	change to lowecase
+	txt = txt.lower()
+	#	remove 'apostraphe s' at the end of words so they are counted with the nonplural version of that word
+	txt = txt.replace("'s", "")
+	#	find beginning of text so that information and table of contents is not included in wordcount
+	temp = txt.split("\n\n\n\n")
+	#for i in temp:
+	#	print(len(i))
+	idx = len(temp[3]) + len(temp[2]) + len(temp[1]) + len(temp[0]) + 4 * len("\n\n\n\n") - 1
+	#	turn into a list of strings with only alphabetical characters
+	txt = re.findall("[a-z]+", txt[idx:])
+
+	#	make alltext.txt
+	g = open("alltext.txt", "w")
+	g.write("\n".join(txt))
+	g.close()
+
+	#	make uniquewords.text using a dictionary and join
+	wordCount = defaultdict(int)
+	for i in txt:
+		wordCount[i] += 1
+	h = open("uniquewords.txt", "w")
+	h.write("\n".join(wordCount.keys()))
+	h.close()
+
+	#	make occuranceCount.txt using another dictionary and a list
+	occuranceCount = defaultdict(int)
+	for i in wordCount.values():
+		occuranceCount[i] += 1
+	occuranceString = []
+	for i,j in occuranceCount.items():
+		occuranceString.append((str(i) + " : " + str(j)))
+	k = open("wordfrequency.txt", "w")
+	k.write("\n".join(occuranceString))
+	k.close()
+
+main()
